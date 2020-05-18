0bbee7517b3f2fe1f4e250c9c74e072362bb466b
fjrd <alexander_reztsov@mail.ru>
2020-05-18 07:01:44 +0300

    try WatchService

--- src/main/java/ru/fjrd/receiptAnalyzer/Main.java
+++ src/main/java/ru/fjrd/receiptAnalyzer/Main.java
@@ -1,16 +1,16 @@
 package ru.fjrd.receiptAnalyzer;
 
 import ru.fjrd.receiptAnalyzer.db.ConnectionFactory;
+import ru.fjrd.receiptAnalyzer.scanner.ReceiptsFolderScanner;
 
 import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) throws IOException, SQLException {
-        File directory = new File("/home/fjrd/Dropbox/Receipts/");
-        for (File file : directory.listFiles()){
-            System.out.println(file.getName());
-        }
+    public static void main(String[] args) throws IOException, SQLException, InterruptedException {
+        ReceiptsFolderScanner receiptsFolderScanner = new ReceiptsFolderScanner();
+        receiptsFolderScanner.loadProperties("src/main/resources/configuration.properties");
+        receiptsFolderScanner.createWatchService();
     }
 }
--- src/main/java/ru/fjrd/receiptAnalyzer/scanner/ReceiptsFolderScanner.java
+++ src/main/java/ru/fjrd/receiptAnalyzer/scanner/ReceiptsFolderScanner.java
@@ -2,17 +2,51 @@ package ru.fjrd.receiptAnalyzer.scanner;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.*;
+import java.util.List;
 import java.util.Properties;
 
 public class ReceiptsFolderScanner {
     String newReceiptsFolderPath;
-    String oldReceiptsFolderPath;
+    String processedReceiptsFolderPath;
+
+    public void createWatchService() throws IOException, InterruptedException {
+        // get path object pointing to the directory we wish to monitor
+        Path path = Paths.get(newReceiptsFolderPath);
+
+        // get watch service which will monitor the directory
+        WatchService watcher = path.getFileSystem().newWatchService();
+        // associate watch service with the directory to listen to the event
+        // types
+
+        path.register(watcher, StandardWatchEventKinds.ENTRY_CREATE);
+        System.out.println("Monitoring directory for changes...");
+
+        // listen to events
+        WatchKey watchKey = watcher.take();
+
+        // get list of events as they occur
+        List<WatchEvent<?>> events = watchKey.pollEvents();
+
+        //iterate over events
+        for (WatchEvent event : events) {
+
+            //check if the event refers to a new file created
+            if (event.kind() == StandardWatchEventKinds.ENTRY_CREATE) {
+
+                //print file name which is newly created
+                System.out.println("Created: " + event.context().toString());
+            }
+        }
+    }
+    public ReceiptsFolderScanner() throws IOException {
+    }
 
     public void loadProperties(String path) throws IOException {
         Properties properties = new Properties();
         properties.load(new FileInputStream(path));
-        newReceiptsFolderPath = properties.getProperty("new.path");
-        oldReceiptsFolderPath = properties.getProperty("old.path");
+        newReceiptsFolderPath = properties.getProperty("from.path");
+        processedReceiptsFolderPath = properties.getProperty("to.path");
     }
 
 
